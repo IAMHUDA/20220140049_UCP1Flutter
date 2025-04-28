2ed5d155db2fcc1b671e6dd11473cdcdd1e4c0cd
IAMHUDA <hudamiftahul362@gmail.com>
2025-04-29 05:43:57 +0700

    add appbar detail piket page

--- lib/detail_piket_page.dart
+++ lib/detail_piket_page.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
-class DetailPiketPage extends StatefulWidget {
-  const DetailPiketPage({super.key});
+class DetailPiketPage extends StatelessWidget {
+  final String nama;
+  final String tanggal;
+  final String tugas;
+
+  const DetailPiketPage({
+    Key? key,
+    required this.nama,
+    required this.tanggal,
+    required this.tugas,
+  }) : super(key: key);
 
   @override
-  State<DetailPiketPage> createState() => _DetailPiketPageState();
-}
-
-class _DetailPiketPageState extends State<DetailPiketPage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFFE5A28),
+          leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          'Detail $tugas', // <-- Ubah jadi dinamis sesuai tugas
+          style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        ),
+      );
+    }
 }
