ffd558e1ebefdae3cb6084793a77e621b0d7a588
IAMHUDA <hudamiftahul362@gmail.com>
2025-04-28 23:24:01 +0700

    add controller & intl date

--- lib/piket_page.dart
+++ lib/piket_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 class PiketPage extends StatefulWidget {
   const PiketPage({super.key});
 
@@ -8,6 +9,20 @@ class PiketPage extends StatefulWidget {
 }
 
 class _PiketPageState extends State<PiketPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _tugasController = TextEditingController();
+  final TextEditingController _tanggalController = TextEditingController();
+
+  List<Map<String, dynamic>> _daftarTugas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting('id_ID', null);
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
