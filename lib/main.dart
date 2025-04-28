55e76adf2eefb02f8f6e65c5e53fa1cde8f24c57
IAMHUDA <hudamiftahul362@gmail.com>
2025-04-28 22:22:35 +0700

    add login pag

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:ucp1/login_page.dart';
 
 
 void main() {
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/login',
       routes: {
-
+        '/login': (context) => const LoginPage(),
       },
       debugShowCheckedModeBanner: false,
     );
