e288e352a0f191810b74e3ec768faa6ac43a6975
IAMHUDA <hudamiftahul362@gmail.com>
2025-04-28 22:50:03 +0700

    add image logo consina ,add form input nama ,add input decoration

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -22,6 +22,51 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(12),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Image.asset('assets/Consina.png', width: 180, height: 200),
+
+                const Text(
+                  "DAFTAR AKUN BARU",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 32),
+
+                // Nama Lengkap
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: const Text("Nama Lengkap"),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: nameController,
+                  decoration: inputDecoration("Nama Lengkap", Icons.person),
+                  validator: (value) =>
+                      value!.isEmpty ? "Nama lengkap tidak boleh kosong" : null,
+                ),
+                const SizedBox(height: 24),
+              ],
+            ),),
+        )),
+    );
+  }
+  InputDecoration inputDecoration(String hint, IconData icon) {
+    return InputDecoration(
+      hintText: hint,
+      prefixIcon: Icon(icon, color: Colors.brown[700]),
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: const BorderSide(color: Colors.red),
+      ),
+      errorStyle: const TextStyle(color: Colors.red),
+    );
   }
 }
