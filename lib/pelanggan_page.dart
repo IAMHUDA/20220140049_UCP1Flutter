32a34826c4df62b5083954c67bf57b246302faee
IAMHUDA <hudamiftahul362@gmail.com>
2025-04-29 06:26:33 +0700

    add app bar

--- lib/pelanggan_page.dart
+++ lib/pelanggan_page.dart
@@ -8,8 +8,41 @@ class PelangganPage extends StatefulWidget {
 }
 
 class _PelangganPageState extends State<PelangganPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _noHpController = TextEditingController();
+  final TextEditingController _alamatController = TextEditingController();
+  final TextEditingController _provinsiController = TextEditingController();
+  final TextEditingController _kodePosController = TextEditingController();
+
+  void _resetForm() {
+    _formKey.currentState?.reset();
+    _namaController.clear();
+    _emailController.clear();
+    _noHpController.clear();
+    _alamatController.clear();
+    _provinsiController.clear();
+    _kodePosController.clear();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFFE5A28),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          'Data Pelanggan',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
