45b955e0e82afd5552335d6b0bca88c99be25a47
IAMHUDA <hudamiftahul362@gmail.com>
2025-04-29 05:40:02 +0700

    add detail piket page

---
+++ lib/detail_piket_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailPiketPage extends StatefulWidget {
+  const DetailPiketPage({super.key});
+
+  @override
+  State<DetailPiketPage> createState() => _DetailPiketPageState();
+}
+
+class _DetailPiketPageState extends State<DetailPiketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
