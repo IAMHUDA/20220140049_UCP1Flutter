a6d08d362966c7c6f30a99b9c182c8d578bbe2c5
IAMHUDA <hudamiftahul362@gmail.com>
2025-04-29 06:43:25 +0700

    add add route page ,adn add page detailpelanggan page

---
+++ lib/detail_pelanggan_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class DetailPelangganPage extends StatelessWidget {
+  final String nama;
+  final String email;
+  final String noHp;
+  final String alamat;
+  final String provinsi;
+  final String kodePos;
+
+  const DetailPelangganPage({
+    super.key,
+    required this.nama,
+    required this.email,
+    required this.noHp,
+    required this.alamat,
+    required this.provinsi,
+    required this.kodePos,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFDF8F5),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFFE5A28),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          'Detail $nama',
+          style: const TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
