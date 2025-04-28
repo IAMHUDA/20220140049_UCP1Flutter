2a7407bd2881096484de34cfa9b4ef15806fe4d0
IAMHUDA <hudamiftahul362@gmail.com>
2025-04-28 22:42:35 +0700

    add controller

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -8,6 +8,18 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  bool _obscurePassword = true;
+  bool _obscureConfirm = true;
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
