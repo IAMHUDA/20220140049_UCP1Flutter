9640b38a8c4aeea0170b337fdb251e7bb7a10818
IAMHUDA <hudamiftahul362@gmail.com>
2025-04-29 07:31:20 +0700

    add deklare data barang page

--- lib/detail_data_barang_page.dart
+++ lib/detail_data_barang_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
 class DetailDataBarangPage extends StatelessWidget {
-  const DetailDataBarangPage({super.key});
+  final String tanggal;
+  final String jenisTransaksi;
+  final String jenisBarang;
+  final int jumlah;
+  final int hargaSatuan;
+  final int totalHarga;
+  
+  const DetailDataBarangPage({
+    super.key,
+    required this.tanggal,
+    required this.jenisTransaksi,
+    required this.jenisBarang,
+    required this.jumlah,
+    required this.hargaSatuan,
+    required this.totalHarga,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      
+    );
   }
 }
