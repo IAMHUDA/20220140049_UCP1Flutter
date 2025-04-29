6f868f3a5a59376e6fb464c882009e5ab77234a5
IAMHUDA <hudamiftahul362@gmail.com>
2025-04-29 07:19:17 +0700

    add deklare harga barang, dan ubah nama hari & bulan ke bahasa indonesia

--- lib/data_barang_page.dart
+++ lib/data_barang_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DataBarangPage extends StatefulWidget {
   const DataBarangPage({super.key});
@@ -8,6 +9,60 @@ class DataBarangPage extends StatefulWidget {
 }
 
 class _DataBarangPageState extends State<DataBarangPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _tanggalController = TextEditingController();
+  final TextEditingController _jumlahController = TextEditingController();
+  final TextEditingController _hargaController = TextEditingController();
+
+  String? _selectedJenisTransaksi;
+  String? _selectedJenisBarang;
+
+  final Map<String, int> _hargaBarang = {
+    'Carrier': 540000,
+    'Sleeping Bag': 250000,
+    'Tenda': 700000,
+    'Sepatu': 350000,
+  };
+
+  // Daftar nama hari dalam bahasa Indonesia
+  final Map<String, String> _indonesianDays = {
+    'Monday': 'Senin',
+    'Tuesday': 'Selasa',
+    'Wednesday': 'Rabu',
+    'Thursday': 'Kamis',
+    'Friday': 'Jumat',
+    'Saturday': 'Sabtu',
+    'Sunday': 'Minggu',
+  };
+
+  Future<void> _selectDate() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null) {
+      setState(() {
+        // Format tanggal dengan hari dalam bahasa Inggris dulu
+        String englishFormat = DateFormat('EEEE/MM/yyyy').format(picked);
+        // Ambil bagian hari dan ganti dengan bahasa Indonesia
+        String englishDay = englishFormat.split('/')[0];
+        String indonesianDay = _indonesianDays[englishDay] ?? englishDay;
+        // Gabungkan kembali dengan format yang sama
+        _tanggalController.text = englishFormat.replaceFirst(englishDay, indonesianDay);
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _tanggalController.dispose();
+    _jumlahController.dispose();
+    _hargaController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
